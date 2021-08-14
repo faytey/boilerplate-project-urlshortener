5338e9f1670b61b598c9dc9c3fa4b51d2ecc534c
dwisatriow <31796624+dwisatriow@users.noreply.github.com>
2021-08-14 17:29:35 +0700

    Add body-parser and url shortener api

--- server.js
+++ server.js
@@ -1,24 +1,35 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+// URL Shortener API
+app.post("/api/shorturl", function (req, res) {
+  res.json({
+    original_url: req.body.url,
+    short_url: req.body.url,
+  });
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
