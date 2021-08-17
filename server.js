5d4b65711fede7bcf0d405ce5ca1f7a8168b3c37
dwisatriow <31796624+dwisatriow@users.noreply.github.com>
2021-08-17 21:09:25 +0700

    Update url shortener api to handle post request

--- server.js
+++ server.js
@@ -1,31 +1,39 @@
+// setup server
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
 
-// Basic configuration
+// setup mongoose
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+// basic configuration
 const port = process.env.PORT || 3000;
 
-// Initiate mongodb connection
+// init mongodb connection
 mongoose.connect(process.env["MONGO_URI"], {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+// checking connection
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function () {
   console.log("Database connected");
 });
 
+// init cors and body parser
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// serve public as static files
 app.use("/public", express.static(`${process.cwd()}/public`));
 
+// serve home page
 app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
@@ -35,14 +43,43 @@ app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
 });
 
+// Setup mongoose schema and model
+const shortUrlSchema = new Schema({
+  originalUrl: String,
+  shortUrl: String,
+});
+
+const ShortUrl = mongoose.model("ShortUrl", shortUrlSchema);
+
+// function to generate random shorturl
+function makeid(length) {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
 // URL shortener API
 app.post("/api/shorturl", function (req, res) {
-  res.json({
-    original_url: req.body.url,
-    short_url: req.body.url,
+  const newUrl = new ShortUrl({
+    originalUrl: req.body.url,
+    shortUrl: makeid(8),
+  });
+
+  newUrl.save(function (err, newUrl) {
+    if (err) return console.error(err);
+    res.json({
+      original_url: req.body.url,
+      short_url: newUrl.shortUrl,
+    });
   });
 });
 
+// listen to port
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
